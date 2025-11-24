9978072103c6d334a42d37ec2381e57e23defbee
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-11-24 14:06:38 +0900

    set up

---
+++ main.js
@@ -0,0 +1,56 @@
+class Node {
+    constructor() {
+        right = null;
+        left = null;
+    }
+}
+
+class Tree {
+    constructor() {
+        root = null;
+    }
+
+    buildTree(array) {
+
+    }
+
+    insert(value) {
+
+    }
+
+    deleteItem(value) {
+
+    }
+
+    find(value) {
+
+    }
+
+    levelOrderForEach(callback) {
+
+    }
+
+    inOrderForEach(callback) {
+
+    }
+
+    preOrderForEach(callback) {
+
+    }
+
+    height(value) {
+
+    }
+
+    depth(value) {
+
+    }
+
+    isBalanced() {
+       
+    }
+
+    rebalance() {
+        
+    }
+}
